3696d0bf6fbbb5acb1518e1c48ad3de708b63dde
Ziaulhaq Parsa Karimi <scholarship327@gmail.com>
2025-12-02 18:54:24 +0500

    fix taskboard

--- src/pages/TaskBoard/TaskBoard.tsx
+++ src/pages/TaskBoard/TaskBoard.tsx
@@ -2,23 +2,47 @@ import { Calendar, Search } from "lucide-react"
 import Section from "../../components/Section/Section"
 import { mockTasks } from "../../lib/consts/mockTasks/mockTask.data";
 import { Input } from "../../components/Search/Input";
-const categories = ['All', 'Gig', 'Internship', 'Tutoring', 'Project', 'Freelance'];
+import { Link, useSearchParams } from "react-router-dom";
+import { useState } from "react";
+import clsx from "clsx";
+import { ScrollToTop } from "../../components/ScrollToTop/ScrollToTop";
+
 
 
 export const TaskBoard = () => {
+  const categories = ['All', 'Gig', 'Internship', 'Tutoring', 'Project', 'Freelance'];
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchTerm, setSearchTerm] = useState('');
+  const selectedCategory = searchParams.get('category') || 'All';
+
+  const filteredTasks = mockTasks.filter((task) => {
+    const matchCategory = selectedCategory === "All" || task.category === selectedCategory;
+    const matchSearch = task.title.toLowerCase().includes(searchTerm.toLowerCase()) || task.description.toLowerCase().includes(searchTerm.toLowerCase());
+    return matchCategory && matchSearch;
+  })
+
   return (
     <Section>
+      <ScrollToTop/>
       <h1 className="text-center mt-25 sm:mt-32 mb-3 text-2xl sm:text-4xl xl:text-5xl font-bold">Browse <span className="text-[var(--primary)]">Opportunities</span></h1>
       <p className="text-center text-sm sm:text-base xl:text-lg tracking-tight mb-10">Explore a variety of tasks and projects to enhance your skills.</p>
       {/* Search and Filter */}
       <div className="mb-8">
         <div className="relative max-w-lg mx-auto">
           <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 size-5"/>
-          <Input className="pl-10" placeholder="Search tasks..."/>
+          <Input
+           value={searchTerm}
+           onChange={(e) => setSearchTerm(e.target.value)}
+           className="pl-10" placeholder="Search tasks..."/>
         </div>
+        {/* Categories Filter */}
         <div className="max-w-lg mx-auto mt-4 flex flex-wrap gap-2">
           {categories.map((category) => (
-            <button key={category} className="border-b border-transparent pb-2 px-4 text-xs sm:text-sm font-medium text-muted-foreground transition-colors hover:border-b hover:border-b-[var(--primary)]">
+            <button key={category}
+             onClick={() => setSearchParams(category === 'All' ? {} : {category})}
+
+             className={ clsx(` ${selectedCategory === category ? 'bg-[var(--primary)]' : 'bg-transparent border border-[var(--primary)]'} pb-2 px-4 text-xs sm:text-sm font-medium 
+                            text-muted-foreground transition-colors cursor-pointer`)}>
               {category}
             </button>
           ))}
@@ -27,11 +51,14 @@ export const TaskBoard = () => {
       {/* Task List */}
       <div>
         {/* task found  */}
-        <h3 className="mb-6">6 opportunities found</h3>
+        <div>
+        <h3 className="mb-6"> {filteredTasks.length} {filteredTasks.length === 1 ? 'Opportunity' : 'Opportunities'} Found</h3>
+        </div>
         {/* Task Cards Grid */}
         <div className="grid grid-cols-1 sm:grid-cols-3 xl:grid-cols-4 gap-2 sm:gap-4">
-          {mockTasks.map((task) => (
-            <div key={task.id} className="card max-sm:p-3">
+          {filteredTasks.map((task) => (
+            <Link to={`/tasks/${task.id}`} key={task.id}>
+            <div className="card max-sm:p-3">
               <div className="flex justify-end items-center mb-3">
                 <span className="text-xs sm:text-sm bg-[var(--success)] text-[var(--text-primary)] rounded-xl px-2 py-0.5 font-semibold ">{task.category}</span>
               </div>
@@ -48,10 +75,16 @@ export const TaskBoard = () => {
               {/* details button  */}
               <button className="w-full mt-4 sm:py-2 bg-[var(--primary)] rounded-md text-sm sm:text-base font-medium hover:bg-[var(--primary-dark)] transition-colors">View Details</button> 
             </div>
+              </Link>
           ))}
         </div>
+        {/* no task found  */}
+        {filteredTasks.length === 0 && (
+           <div className="text-center py-12">
+            <p className="text-xl text-gray-600">No tasks found. Try adjusting your filters.</p>
+          </div>
+        )}
       </div>
     </Section>
   )
 }
-
