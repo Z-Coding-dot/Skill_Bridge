c4611557c14cc664a99a6c59a988a6304fb7f2b7
Ziaulhaq Parsa Karimi <scholarship327@gmail.com>
2025-12-16 17:09:36 +0500

    I worked on api and dashgoards components

--- src/components/ui/Dashboard/MyTask.tsx
+++ src/components/ui/Dashboard/MyTask.tsx
@@ -3,20 +3,27 @@ import Section from "@/components/Section/Section";
 import { useQuery } from "@tanstack/react-query";
 import type { Task } from "@/schemas/task.schema";
 import { getMyTasks } from "@/api/tasks.api";
+import { useState } from "react";
+import { AddTaskModal } from "@/components/Modal/AddTaskModal";
 
 
 
 export const MyTask = () => {
-  const { data, isLoading } = useQuery<Task[]>({
-     queryKey: ["my_task"],
+  const [open, setOpen] = useState<boolean>(false);
+  const { data, isLoading, error } = useQuery<Task[]>({
+     queryKey: ["my_tasks"],
      queryFn: getMyTasks,
    });
+
+   if(error){
+    return <Section><div className="text-center mt-25 text-red-500 text-4xl font-bold">Something went wrong 😟</div></Section>
+   }
 
   if (isLoading) {
     return <Section>Loading...</Section>;
   }
 
- if (data?.length === 0) {
+ if ( !data ||  data?.length === 0) {
     return (
         <div className="2xl:max-w-4xl mx-auto flex flex-col items-center justify-center bg-2card w-full rounded-2xl mt-20 p-12">
           <Briefcase className="size-12 mb-8" />
@@ -31,22 +38,25 @@ export const MyTask = () => {
     <Section>
       <div className="flex items-center justify-between mt-5">
         <h1 className="2xl:text-xl font-semibold">My Posted Tasks</h1>
-        <button className="flex items-center gap-2 2xl:text-base">
+        <button onClick={() => setOpen((prev) => !prev)} className="flex items-center gap-2 2xl:text-base">
           <Plus className="size-5" /> Add Task
         </button>
       </div>
-      {data?.map((task) => (
-        <ul key={task.id} className="mt-10 space-y-4">
-            <div className="bg-2card p-4 rounded-xl">
-                <div>
-                  <h2>{task.title}</h2>
-                  <p>{task.description}</p>
-                
-                </div>
-            </div>
-        </ul>
+      {open && <AddTaskModal isOpen={open} onClose={() => setOpen((pre) => !pre)} />}
+      {data && data.length > 0 && ( 
+    <ul className="mt-10 space-y-4">
+    {data.map((task) => (
+      <li key={task.id} className="bg-2card p-4 rounded-xl">
+        <h2 className="font-semibold">{task.title}</h2>
+        <p className="text-sm text-stone-400">{task.description}</p>
+        <span className="text-xs text-stone-500">
+          {task.category} • {task.status}
+        </span>
+      </li>
+    ))}
+  </ul>
+  )}
 
-      ))}
       
     </Section>
   );
